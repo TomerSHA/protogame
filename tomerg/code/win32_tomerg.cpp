f904741b5241a92460a105389af7617874a574ed
TomerSHA <shaimtom@post.bgu.ac.il>
2016-07-02 08:12:27 +0300

    "First commit - skeleton"

---
+++ tomerg/code/win32_tomerg.cpp
@@ -0,0 +1,17 @@
+/*=========================
+	$File: $
+	$Date: $
+	$Revision: $
+	$Creator: $Tomer Shaim
+	================================*/
+
+#include <windows.h>
+
+int CALLBACK WinMain(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nCmdShow)
+{
+  return(0);
+}
